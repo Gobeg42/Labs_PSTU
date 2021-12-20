139d826f546bd47d3901f7757e619099fd580de6
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2021-12-21 00:16:37 +0500

    Add files via upload

---
+++ 3.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a[20];
+	int key;
+	int r, range_max, range_min;
+	range_min = 2;
+	range_max = 412;
+	for (int i = 0; i < 20; i++)
+	{
+		a[i] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	for (int j = 1; j < 20; j++)
+	{
+		key = a[j];
+		int i = j - 1;
+		while (i >= 0 && a[i] > key)
+		{
+			a[i + 1] = a[i];
+			i--;
+		}
+		a[i + 1] = key;
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
