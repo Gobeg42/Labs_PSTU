ec43d49669fa85aba27d08ad10dadff12011bb72
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2021-12-20 12:26:24 +0500

    Add files via upload

---
+++ 2.16.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, z, sum = 0;
+	cin >> n;
+	cin >> z;
+	for (int i = 0; i <= n; i++)
+	{
+		if (i % 2 == 0 and i % z == 0)
+		{
+			sum += i;
+		}
+		if (i % 2 != 0 and i % z == 0)
+		{
+			sum -= i;
+		}
+	}
+	cout << sum;
+	return 0;
+}
