951c0c35dce7ffb462e8c0dcaae73428e71737aa
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2021-12-21 23:57:50 +0500

    Add files via upload

---
+++ половинный.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// f(x) = 2x^3 - 3x - 16
+// f'(x) = 6x^2-3
+// [1, 2]
+// f(1) = 8 - 3 - 16 = -11
+// f(1) * f(2) < 0
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float a, b, c;
+    float fA, fB, fC;
+    double eps = 0.000001;
+    a = 0;
+    b = 3;
+    while (b - a > eps)
+    {
+        c = (a + b) / 2;
+        fA = (2*(pow(a, 3)) - (3*a) - 16);
+        fB = (2*(pow(b, 3)) - (3*b) - 16);
+        fC = (2*(pow(c, 3)) - (3*c) - 16);
+        if (fA * fC < 0) { b = c; }
+        else if (fC * fB < 0) { a = c; }
+        else { cout << "Что-то не то!" << endl; }
+    }
+    cout << "Корни: " << a << "; " << b << endl;
+    return 0;
+}
+
+
