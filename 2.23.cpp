a4f7cc973b531d3c63dea475f9526738c3d50556
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2021-12-21 14:37:16 +0500

    Add files via upload

---
+++ 2.23.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL("rus"));
+	float n, max;
+	int count = 0;
+	cin >> n;
+	max = -1;
+	for (float i = 1; i <= n; i++)
+	{
+		cout << (round(sin(n + (i / n)) * 10) / 10) << endl;
+		if ((round(sin(n + (i / n)) * 10) / 10) > max)
+		{
+			max = (round(sin(n + (i / n)) * 10) / 10);
+		}
+	}
+
+	for (float i = 1; i <= n; i++)
+	{
+		if ((round(sin(n + (i / n)) * 10) / 10) == max)
+		{
+			count++;
+		}
+	}
+	cout << "Max: " << max << endl;
+	cout << "Numbers of equal to Max: " << count;
+}
