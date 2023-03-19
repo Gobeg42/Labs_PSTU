732556baacdbda1a8cea0b3342e81a2f2bcc80bd
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2023-03-20 04:17:28 +0500

    Add files via upload

---
+++ Сортировка выбор/Сортировка выбором.cpp
@@ -0,0 +1,26 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    int const SIZE = 7;
+    int arr[SIZE]{ 1,5,0,4,33,19,-2 };
+    for (int i = 0; i < SIZE ; i++)
+    {
+        int min = i;
+        for (int j = i+1 ; j < SIZE; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j; 
+            }
+            swap(arr[i], arr[min]);
+
+           
+        }
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
