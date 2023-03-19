0203d897d1cc63ac6dcd5d7ea1e688597e10fe65
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2023-03-20 04:57:24 +0500

    Add files via upload

---
+++ Сортировка вставкой/Сортировка вставкой.cpp
@@ -0,0 +1,30 @@
+﻿
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int const SIZE = 6;
+    int arr[] = { -3,0,-5,12,8,3 };
+    int j;
+   
+    for (int i = 1; i < SIZE; i++)
+    {
+       int tmp = arr[i];
+        for (j = i - 1; j >= 0 && arr[j] > tmp; j--)
+        {
+            arr[j + 1] = arr[j];
+            arr[j] = -9999;
+        }
+        arr[j + 1] = tmp;
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+
+}
