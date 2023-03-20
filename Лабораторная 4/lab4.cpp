eebc83da33b37df89077934b3da30676d2234a66
Gobeg42 <90241025+Gobeg42@users.noreply.github.com>
2023-03-20 20:12:54 +0500

    Add files via upload

---
+++ Лабораторная 4/lab4.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int const SIZE = 5;
+    int arr[SIZE];
+    int maxi, mini;
+    for (int i = 0; i < SIZE; i++)//заполняю массив рандомными значениями
+    {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        maxi = i;
+        mini = i;
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (arr[j] > arr[maxi])
+                maxi = j;
+            if (arr[j] < arr[mini])
+                mini = j;
+        }
+        swap(arr[maxi], arr[mini]);//меняю максимальное и минимальное значение местами
+    }
+    int sum = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        sum = sum + arr[i];//суммирую все числа в массиве
+    }
+    float sr = (float)sum / SIZE;//среднее значение
+    for (int i = 0; i < SIZE; i++)
+    {
+        if ((arr[i] + sr / 10) < sr)
+            cout << arr[i] << " ";
+    }
+}
